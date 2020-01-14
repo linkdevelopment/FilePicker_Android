effab09f805185c50d4fba5c0350921ab8072143
Mohamed Abdel Mohsen <mabdelmohsen222@gmail.com>
2020-01-14 16:51:02 +0200

    add file directory

--- app/src/main/java/com/linkdev/filepicker_android/pickFilesComponent/FileUtils.kt
+++ app/src/main/java/com/linkdev/filepicker_android/pickFilesComponent/FileUtils.kt
@@ -4,6 +4,9 @@ import android.content.ContentResolver
 import android.content.Context
 import android.database.Cursor
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Matrix
+import android.media.ExifInterface
 import android.net.Uri
 import android.os.Build
 import android.os.Environment
@@ -12,8 +15,8 @@ import android.provider.OpenableColumns
 import android.util.Log
 import android.webkit.MimeTypeMap
 import androidx.core.content.FileProvider
+import com.linkdev.filepicker_android.BuildConfig
 import com.linkdev.filepicker_android.R
-import com.linkdev.filepicker_android.pickFilesComponent.image.CaptureImage
 import java.io.*
 import java.text.SimpleDateFormat
 import java.util.*
@@ -120,28 +123,7 @@ object FileUtils {
         }
     }
 
-    @Throws(IOException::class)
-    fun createImageFile(context: Context): File {
-        // Create an image file name
-        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.ENGLISH).format(Date())
-        val storageDir: File? = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
-        return File.createTempFile(
-            "${context.getString(R.string.app_name)}_$timeStamp", /* prefix */
-            CAMERA_IMAGE_TYPE, /* suffix */
-            storageDir /* directory */
-        )
-    }
-
-    fun getFileURI(context: Context, providerAuth: String, file: File): Uri {
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            FileProvider.getUriForFile(
-                context, context.packageName + providerAuth, file
-            )
-        } else {
-            Uri.fromFile(file)
-        }
-    }
-
+    // convert bitmap to file
     fun convertBitmapToFile(context: Context, bitmap: Bitmap, quality: Int): File {
         val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.ENGLISH).format(Date())
         val filesDir = context.getFilesDir()
@@ -158,5 +140,193 @@ object FileUtils {
         }
 
         return imageFile
+    }
+
+    fun getAppFolder(): String {
+        val folder = File(
+            Environment.getExternalStorageDirectory().absolutePath
+                    + "/"
+                    + "FilePicker"
+        )
+        folder.mkdirs()
+        return folder.path
+    }
+
+    fun getAppFilePath(fileName: String?) =
+        getAppFolder() + "/" + fileName
+
+    fun createAppFile(fileName: String?): File {
+        return File(getAppFilePath(fileName))
+    }
+
+    fun createImageFile(context: Context, fileName: String): File? {
+        try {
+            val storageDir: File = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+                ?: return null
+            return File(storageDir.path, fileName)
+        } catch (e: Exception) {
+            e.printStackTrace()
+            return null
+        }
+    }
+
+    fun writeBitmapToFile(bitmap: Bitmap, fileName: String): File? {
+        try {
+            val file = createAppFile(fileName)
+            val fOut = FileOutputStream(file)
+
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fOut)
+            fOut.flush()
+            fOut.close()
+            return file
+        } catch (e: Exception) {
+            e.printStackTrace()
+            return null
+        }
+    }
+
+    fun getFileName(filePath: String): String = File(filePath).name
+
+    fun getAdjustedBitmap(filePath: String): Bitmap? {
+        try {
+            val orientation: Int = getOrientation(filePath)
+            var adjustedBitmap: Bitmap? = BitmapFactory.decodeFile(filePath)
+            when (orientation) {
+                ExifInterface.ORIENTATION_ROTATE_90 -> {
+                    adjustedBitmap = rotateImage(adjustedBitmap, 90f)
+                    adjustedBitmap = resizeBitmap(adjustedBitmap)
+                }
+                ExifInterface.ORIENTATION_ROTATE_180 -> {
+                    adjustedBitmap = rotateImage(adjustedBitmap, 180f)
+                    adjustedBitmap = resizeBitmap(adjustedBitmap)
+                }
+                ExifInterface.ORIENTATION_ROTATE_270 -> {
+                    adjustedBitmap = rotateImage(adjustedBitmap, 270f)
+                    adjustedBitmap = resizeBitmap(adjustedBitmap)
+                }
+                ExifInterface.ORIENTATION_NORMAL -> {
+                    adjustedBitmap = resizeBitmap(adjustedBitmap)
+                }
+                else -> {
+                    adjustedBitmap = resizeBitmap(adjustedBitmap)
+                }
+            }
+            return adjustedBitmap
+        } catch (outOfMemoryError: OutOfMemoryError) {
+            outOfMemoryError.printStackTrace()
+            return null
+        } catch (e: Exception) {
+            e.printStackTrace()
+            return null
+        }
+    }
+
+    fun resizeBitmap(bitmap: Bitmap?): Bitmap? {
+        if (bitmap == null)
+            return null
+
+        if (bytesToMegaBytes(bitmap.byteCount) < 10)
+            return bitmap
+
+        val width = bitmap.width
+        val height = bitmap.height
+
+        var returnedBitmap: Bitmap? = null
+        try {
+            returnedBitmap = Bitmap.createScaledBitmap(bitmap, width / 2, height / 2, false)
+            if (returnedBitmap != null && bytesToMegaBytes(bitmap.byteCount) > 10) {
+                returnedBitmap = resizeBitmap(returnedBitmap)
+            }
+        } catch (error: OutOfMemoryError) {
+            error.printStackTrace()
+            returnedBitmap = resizeBitmap(returnedBitmap)
+        }
+
+        return returnedBitmap
+    }
+
+    fun bytesToMegaBytes(bytes: Int): Double {
+        return bytes.toDouble() / 1000000
+    }
+
+    internal fun getOrientation(filePath: String): Int {
+        try {
+            val ei = ExifInterface(filePath)
+            return ei.getAttributeInt(
+                ExifInterface.TAG_ORIENTATION,
+                ExifInterface.ORIENTATION_UNDEFINED
+            )
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+        return ExifInterface.ORIENTATION_UNDEFINED
+    }
+
+    fun rotateImage(filePath: String): Bitmap? {
+        val orientation: Int = getOrientation(filePath)
+        val bitmap: Bitmap? = getBitmap(filePath)
+
+        return when (orientation) {
+            ExifInterface.ORIENTATION_ROTATE_90 -> {
+                rotateImage(bitmap, 90f)
+            }
+            ExifInterface.ORIENTATION_ROTATE_180 -> {
+                rotateImage(bitmap, 180f)
+            }
+            ExifInterface.ORIENTATION_ROTATE_270 -> {
+                rotateImage(bitmap, 270f)
+            }
+            ExifInterface.ORIENTATION_NORMAL -> {
+                return bitmap
+            }
+            else -> {
+                return bitmap
+            }
+        }
+    }
+
+    private fun getBitmap(filePath: String): Bitmap? {
+        try {
+            return BitmapFactory.decodeFile(filePath)
+        } catch (e: OutOfMemoryError) {
+            e.printStackTrace()
+            return resizeBitmap(BitmapFactory.decodeFile(filePath))
+        }
+    }
+
+    fun rotateImage(source: Bitmap?, angle: Float): Bitmap? {
+        if (source == null)
+            return null
+
+        try {
+            val matrix = Matrix()
+            matrix.postRotate(angle)
+            return Bitmap.createBitmap(
+                source, 0, 0, source.width, source.height,
+                matrix, true
+            )
+        } catch (error: OutOfMemoryError) {
+            error.printStackTrace()
+            return null
+        }
+    }
+
+    fun isImageMimeType(mimeType: String): Boolean {
+        return mimeType.startsWith("image")
+    }
+
+    fun geImageName(extension: String = CAMERA_IMAGE_TYPE): String =
+        "IMG_" + SimpleDateFormat(
+            "yyyyMMdd_HHmmss",
+            Locale.ENGLISH
+        ).format(Date()) + extension
+
+    fun getFileUri(context: Context, fileUrl: String): Uri {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            FileProvider.getUriForFile(context, context.packageName + ".provider", File(fileUrl))
+        } else {
+            Uri.fromFile(File(fileUrl))
+        }
     }
 }
--- app/src/main/java/com/linkdev/filepicker_android/pickFilesComponent/image/CaptureImage.kt
+++ app/src/main/java/com/linkdev/filepicker_android/pickFilesComponent/image/CaptureImage.kt
@@ -2,19 +2,14 @@ package com.linkdev.filepicker_android.pickFilesComponent.image
 
 import android.app.Activity
 import android.content.Intent
-import android.graphics.Bitmap
 import android.provider.MediaStore
-import android.util.Log
 import androidx.fragment.app.Fragment
-import com.linkdev.filepicker_android.R
 import com.linkdev.filepicker_android.pickFilesComponent.FileUtils
-import com.linkdev.filepicker_android.pickFilesComponent.PickFileUtils.ErrorStatus.DATA_ERROR
 import com.linkdev.filepicker_android.pickFilesComponent.PickFilesResultCallback
 import com.linkdev.filepicker_android.pickFilesComponent.model.ErrorModel
 import com.linkdev.filepicker_android.pickFilesComponent.model.MimeType
 import com.linkdev.filepicker_android.pickFilesComponent.pickFileFactory.IPickFilesFactory
 import java.io.File
-import java.io.IOException
 
 
 class CaptureImage(private val fragment: Fragment, private val shouldMakeDir: Boolean) :
@@ -25,89 +20,43 @@ class CaptureImage(private val fragment: Fragment, private val shouldMakeDir: Bo
         const val TAG = "FilePickerTag"
         const val CAPTURE_IMAGE_REQUEST_CODE = 1001
         const val DATA_EXTRA = "data"
-        const val PROVIDER_AUTH = ".fileProvider"
+        const val PROVIDER_AUTH = ".provider"
     }
 
     override fun pickFiles(mimeTypeSet: Set<MimeType>, chooserMessage: String) {
         val captureImageIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        if (captureImageIntent.resolveActivity(fragment.requireContext().packageManager) != null) {
-            if (shouldMakeDir) {
-                onShouldMakeDir(captureImageIntent)
-            } else {
-                onShouldMakeNoDir(captureImageIntent)
-            }
-        }
-    }
-
-    // start activity with make dir
-    private fun onShouldMakeDir(intent: Intent) {
-        val imageFile: File? = try {
-            FileUtils.createImageFile(fragment.requireContext())
-        } catch (ex: IOException) {
-            null
-        }
-        imageFile?.let {
-            val photoURI = FileUtils.getFileURI(fragment.requireContext(), PROVIDER_AUTH, it)
-            Log.e(TAG, "photo URI $photoURI")
-            intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
-            fragment.startActivityForResult(intent, CAPTURE_IMAGE_REQUEST_CODE)
-        }
+        val imageFile =
+            FileUtils.createImageFile(fragment.requireContext(), FileUtils.geImageName())
         currentCapturedPath = imageFile?.path
 
-    }
+        val photoURI =
+            currentCapturedPath?.let { FileUtils.getFileUri(fragment.requireContext(), it) }
 
-    // start activity without make dir
-    private fun onShouldMakeNoDir(intent: Intent) {
-        fragment.startActivityForResult(intent, CAPTURE_IMAGE_REQUEST_CODE)
+        photoURI?.let {
+            captureImageIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+            fragment.startActivityForResult(captureImageIntent, CAPTURE_IMAGE_REQUEST_CODE)
+        }
     }
 
     override fun handleActivityResult(
         requestCode: Int, resultCode: Int, data: Intent?, callback: PickFilesResultCallback
     ) {
-        if (resultCode == Activity.RESULT_OK) {
-            if (requestCode == CAPTURE_IMAGE_REQUEST_CODE) {
-                if (shouldMakeDir) {
-                    handleCaptureImageWithDir(callback)
+        if (currentCapturedPath != null) {
+            if (resultCode == Activity.RESULT_OK) {
+                if (requestCode == CAPTURE_IMAGE_REQUEST_CODE) {
+                    val uri =
+                        FileUtils.getFileUri(fragment.requireContext(), currentCapturedPath!!)
+                    val adjustedBitmap = FileUtils.getAdjustedBitmap(currentCapturedPath!!)
+                    val file = adjustedBitmap?.let {
+                        FileUtils.writeBitmapToFile(adjustedBitmap, FileUtils.geImageName())
+                    }
+                    callback.onFilePicked(uri, file?.path, file, adjustedBitmap)
                 } else {
-                    handleCaptureImageWithNoDir(data, callback)
+                    callback.onPickFileCanceled()
                 }
-            } else {
-                callback.onPickFileError(
-                    ErrorModel(DATA_ERROR, R.string.pick_file_data_error)
-                )
             }
         } else {
-            callback.onPickFileCanceled()
-        }
-    }
-
-
-    private fun handleCaptureImageWithDir(callback: PickFilesResultCallback) {
-        if (currentCapturedPath == null) {
-            callback.onPickFileError(ErrorModel(DATA_ERROR, R.string.pick_file_data_error))
-        } else {
-            val file = FileUtils.getFileFromPath(currentCapturedPath)
-            val uri =
-                file?.let { FileUtils.getFileURI(fragment.requireContext(), PROVIDER_AUTH, it) }
-            callback.onFilePicked(uri, currentCapturedPath, file, null)
-        }
-    }
-
-    // handle image without save in custom dir/capture from memory
-    private fun handleCaptureImageWithNoDir(data: Intent?, callback: PickFilesResultCallback) {
-        if (data != null) {
-            if (data.hasExtra(DATA_EXTRA)) {
-                val bitmap = data.extras?.get(DATA_EXTRA) as Bitmap
-                val file =
-                    FileUtils.convertBitmapToFile(fragment.requireContext(), bitmap, 100)
-                callback.onFilePicked(null, file.absolutePath, file, bitmap)
-            } else {
-                callback.onPickFileError(
-                    ErrorModel(DATA_ERROR, R.string.pick_file_data_error)
-                )
-            }
-        } else {
-            callback.onPickFileError(ErrorModel())
+            callback.onPickFileError(ErrorModel("error current pic is null"))
         }
     }
 }
