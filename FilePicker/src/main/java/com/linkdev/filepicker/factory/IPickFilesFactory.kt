9d3f2a4b959eaaa224224e9dadbdc79b234f3b47
mohamed.abdulmohsen <mohamed.abdulmohsen@linkdev.com>
2020-09-27 18:17:04 +0200

    set default mime type as All files

--- FilePicker/src/main/java/com/linkdev/filepicker/factory/IPickFilesFactory.kt
+++ FilePicker/src/main/java/com/linkdev/filepicker/factory/IPickFilesFactory.kt
@@ -5,7 +5,7 @@ import com.linkdev.filepicker.interactions.PickFilesStatusCallback
 import com.linkdev.filepicker.models.MimeType
 
 interface IPickFilesFactory {
-    fun pickFiles(mimeTypeList: ArrayList<MimeType>)
+    fun pickFiles(mimeTypeList: ArrayList<MimeType> = arrayListOf(MimeType.ALL_FILES))
 
     fun handleActivityResult(
         mRequestCode: Int, resultCode: Int, data: Intent?, callback: PickFilesStatusCallback
